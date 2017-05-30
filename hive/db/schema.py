0df4fa3d61bd67fac601f2e1411667810afaae6b
Tim <roadscape@users.noreply.github.com>
2017-05-30 16:38:47 -0400

    typo

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -160,7 +160,7 @@ hive_posts_cache = sa.Table(
     sa.Column('sc_trend', DOUBLE, nullable=False),
     sa.Column('sc_hot', DOUBLE, nullable=False),
     sa.Column('body', sa.Text),
-    sa.Column('votes', MEDIUMTEXT)
+    sa.Column('votes', MEDIUMTEXT),
     sa.Column('json', sa.Text),
     sa.ForeignKeyConstraint(['post_id'], ['hive_posts.id'], name='hive_posts_cache_fk1'),
     sa.Index('hive_posts_cache_ix1', 'payout'),
