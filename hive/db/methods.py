3cbd3881adff3e018bd1f642d990f638594c8745
Tim <roadscape@users.noreply.github.com>
2017-12-13 11:14:12 -0600

    linting

--- hive/db/methods.py
+++ hive/db/methods.py
@@ -1,13 +1,12 @@
 import logging
-from funcy.seqs import first
-from hive.db import conn
-from sqlalchemy import text, select, func
-from decimal import Decimal
-
 import time
 import re
 import atexit
 
+from decimal import Decimal
+from hive.db import conn
+from funcy.seqs import first
+from sqlalchemy import text
 
 class QueryStats:
     stats = {}
@@ -29,7 +28,7 @@ class QueryStats:
     def print(cls):
         ttl = cls.ttltime
         print("[DEBUG] total SQL time: {}s".format(int(ttl / 1000)))
-        for arr in sorted(cls.stats.items(), key=lambda x:-x[1][0])[0:40]:
+        for arr in sorted(cls.stats.items(), key=lambda x: -x[1][0])[0:40]:
             sql, vals = arr
             ms, calls = vals
             print("% 5.1f%% % 10.2fms % 7.2favg % 8dx -- %s" % (100 * ms/ttl, ms, ms/calls, calls, sql[0:180]))
@@ -49,7 +48,7 @@ def query(sql, **kwargs):
         res = conn.execute(query, **kwargs)
         ms = int((time.perf_counter() - ti) * 1000)
         QueryStats.log(sql, ms)
-        if ms > 100:
+        if ms > 25:
             disp = re.sub('\s+', ' ', sql).strip()[:250]
             print("\033[93m[SQL][{}ms] {}\033[0m".format(ms, disp))
         logger.debug(res)
@@ -93,16 +92,16 @@ def db_needs_setup():
 async def db_head_state():
     sql = "SELECT num,created_at,extract(epoch from created_at) ts FROM hive_blocks ORDER BY num DESC LIMIT 1"
     row = query_row(sql)
-    return dict(db_head_block = row['num'],
-                db_head_time = str(row['created_at']),
-                db_head_age = int(time.time() - row['ts']))
+    return dict(db_head_block=row['num'],
+                db_head_time=str(row['created_at']),
+                db_head_age=int(time.time() - row['ts']))
 
 
 async def db_last_block():
     return query_one("SELECT MAX(num) FROM hive_blocks") or 0
 
 def get_account_id(name):
-    return query_one("SELECT id FROM hive_accounts WHERE name = :n", n = name)
+    return query_one("SELECT id FROM hive_accounts WHERE name = :n", n=name)
 
 # api specific
 # ------------
@@ -115,7 +114,7 @@ async def get_followers(account: str, skip: int, limit: int):
     ORDER BY hf.created_at DESC LIMIT :limit OFFSET :skip
     """
     res = query(sql, account_id=account_id, skip=int(skip), limit=int(limit))
-    return [[r[0],str(r[1])] for r in res.fetchall()]
+    return [[r[0], str(r[1])] for r in res.fetchall()]
 
 
 async def get_following(account: str, skip: int, limit: int):
@@ -127,7 +126,7 @@ async def get_following(account: str, skip: int, limit: int):
     ORDER BY hf.created_at DESC LIMIT :limit OFFSET :skip
     """
     res = query(sql, account_id=account_id, skip=int(skip), limit=int(limit))
-    return [[r[0],str(r[1])] for r in res.fetchall()]
+    return [[r[0], str(r[1])] for r in res.fetchall()]
 
 
 async def following_count(account: str):
@@ -143,7 +142,7 @@ async def follower_count(account: str):
 # evaluate replacing two above methods with this
 async def follow_stats(account: str):
     sql = "SELECT following, followers FROM hive_accounts WHERE name = :account"
-    return first(query(sql))
+    return first(query(sql, account=account))
 
 # all completed payouts
 async def payouts_total():
@@ -171,7 +170,7 @@ async def payouts_last_24h():
 
 
 # given an array of post ids, returns full metadata in the same order
-def get_posts(ids, context = None):
+def get_posts(ids, context=None):
     sql = """
     SELECT post_id, author, permlink, title, preview, img_url, payout,
            promoted, created_at, payout_at, is_nsfw, rshares, votes, json
@@ -210,15 +209,15 @@ def get_posts(ids, context = None):
     missed = set(ids) - posts_by_id.keys()
     if missed:
         print("WARNING: get_posts do not exist in cache: {}".format(missed))
-        for id in missed:
-            ids.remove(id)
+        for _id in missed:
+            ids.remove(_id)
 
-    return [posts_by_id[id] for id in ids]
+    return [posts_by_id[_id] for _id in ids]
 
 
 # builds SQL query to pull a list of posts for any sort order or tag
 # sort can be: trending hot new promoted
-async def get_discussions_by_sort_and_tag(sort, tag, skip, limit, context = None):
+async def get_discussions_by_sort_and_tag(sort, tag, skip, limit, context=None):
     if skip > 5000:
         raise Exception("cannot skip {} results".format(skip))
     if limit > 100:
@@ -266,7 +265,7 @@ async def get_user_feed(account: str, skip: int, limit: int, context: str = None
     GROUP BY post_id
     ORDER BY MIN(hive_feed_cache.created_at) DESC LIMIT :limit OFFSET :skip
     """
-    res = query_all(sql, account = account_id, skip = skip, limit = limit)
+    res = query_all(sql, account=account_id, skip=skip, limit=limit)
     posts = get_posts([r[0] for r in res], context)
 
     # Merge reblogged_by data into result set
@@ -284,7 +283,7 @@ async def get_user_feed(account: str, skip: int, limit: int, context: str = None
 async def get_blog_feed(account: str, skip: int, limit: int, context: str = None):
     account_id = get_account_id(account)
     sql = ("SELECT post_id FROM hive_feed_cache WHERE account_id = :account_id "
-            "ORDER BY created_at DESC LIMIT :limit OFFSET :skip")
+           "ORDER BY created_at DESC LIMIT :limit OFFSET :skip")
     post_ids = query_col(sql, account_id=account_id, skip=skip, limit=limit)
     return get_posts(post_ids, context)
 
@@ -302,5 +301,3 @@ async def get_related_posts(account: str, permlink: str):
     thresh = time.time() / 480000
     post_ids = query_col(sql, a=account, p=permlink, t=thresh)
     return get_posts(post_ids)
-
-
