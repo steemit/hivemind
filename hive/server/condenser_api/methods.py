62a641372ac88b7140d67c3a50597104df699918
roadscape <roadscape@users.noreply.github.com>
2018-06-04 12:46:42 -0500

    pull out account and post factory

--- hive/server/condenser_api/methods.py
+++ hive/server/condenser_api/methods.py
@@ -1,14 +1,15 @@
 """Steemd/condenser_api compatibility layer API methods."""
 
 import json
-import inspect
 import re
 import collections
 
 from functools import wraps
 from aiocache import cached
+
 from hive.db.methods import query_one, query_row, query_col, query_all
 from hive.utils.normalize import parse_amount
+from hive.server.condenser_api.objects import load_accounts, load_posts
 import hive.server.condenser_api.cursor as cursor
 
 def _strict_list(params, expected_len):
@@ -122,6 +123,34 @@ async def get_follow_count(account: str):
                 follower_count=count['followers'])
 
 
+# Content Primitives
+
+async def get_content(author: str, permlink: str):
+    """Get a single post object."""
+    _validate_account(author)
+    _validate_permlink(permlink)
+    post_id = _get_post_id(author, permlink)
+    if not post_id:
+        return {'id': 0, 'author': '', 'permlink': ''}
+    return load_posts([post_id])[0]
+
+
+async def get_content_replies(parent: str, parent_permlink: str):
+    """Get a list of post objects based on parent."""
+    _validate_account(parent)
+    _validate_permlink(parent_permlink)
+    post_id = _get_post_id(parent, parent_permlink)
+    if not post_id:
+        return []
+    post_ids = query_col("SELECT id FROM hive_posts WHERE "
+                         "parent_id = %d AND is_deleted = '0'" % post_id)
+    if not post_ids:
+        return []
+    return load_posts(post_ids)
+
+
+# Discussion Queries
+
 def nested_query_compat(function):
     """Unpack strange format used by some clients, accepted by steemd.
 
@@ -154,7 +183,7 @@ async def get_discussions_by_trending(start_author: str = '', start_permlink: st
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 20),
         tag)
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 
 @nested_query_compat
@@ -168,7 +197,7 @@ async def get_discussions_by_hot(start_author: str = '', start_permlink: str = '
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 20),
         tag)
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 
 @nested_query_compat
@@ -182,7 +211,7 @@ async def get_discussions_by_promoted(start_author: str = '', start_permlink: st
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 20),
         tag)
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 
 @nested_query_compat
@@ -196,7 +225,7 @@ async def get_discussions_by_created(start_author: str = '', start_permlink: str
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 20),
         tag)
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 
 @nested_query_compat
@@ -209,7 +238,7 @@ async def get_discussions_by_blog(tag: str, start_author: str = '',
         _validate_account(start_author, allow_empty=True),
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 20))
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 
 @nested_query_compat
@@ -224,7 +253,7 @@ async def get_discussions_by_feed(tag: str, start_author: str = '',
         _validate_limit(limit, 20))
 
     reblogged_by = dict(res)
-    posts = _get_posts([r[0] for r in res], truncate_body=truncate_body)
+    posts = load_posts([r[0] for r in res], truncate_body=truncate_body)
 
     # Merge reblogged_by data into result set
     for post in posts:
@@ -244,7 +273,7 @@ async def get_discussions_by_comments(start_author: str, start_permlink: str = '
         _validate_account(start_author),
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 20))
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 
 @nested_query_compat
@@ -255,7 +284,7 @@ async def get_replies_by_last_update(start_author: str, start_permlink: str = ''
         _validate_account(start_author),
         _validate_permlink(start_permlink, allow_empty=True),
         _validate_limit(limit, 50))
-    return _get_posts(ids, truncate_body=truncate_body)
+    return load_posts(ids, truncate_body=truncate_body)
 
 def _normalize_path(path):
     if path[0] == '/':
@@ -278,7 +307,7 @@ def _keyed_posts(posts):
 
 def _load_posts_accounts(posts):
     names = set(map(lambda p: p['author'], posts))
-    accounts = _load_accounts(names)
+    accounts = load_accounts(names)
     return {a['name']: a for a in accounts}
 
 async def get_state(path: str):
@@ -304,7 +333,7 @@ async def get_state(path: str):
         assert not part[2], 'unexpected account path[2] %s' % path
 
         account = _validate_account(part[0][1:])
-        state['accounts'][account] = _load_accounts([account])[0]
+        state['accounts'][account] = load_accounts([account])[0]
 
         # dummy paths used by condenser - just need account object
         ignore = ['followed', 'followers', 'permissions',
@@ -344,9 +373,9 @@ async def get_state(path: str):
         assert not part[2], "unexpected discussion path part[2] %s" % path
         sort = _validate_sort(part[0])
         tag = _validate_tag(part[1].lower(), allow_empty=True)
-        posts = _get_posts(cursor.pids_by_query(sort, '', '', 20, tag))
-        state['content'] = list(_keyed_posts(posts))
-        state['discussion_idx'][tag][sort] = state['content'].keys()
+        posts = load_posts(cursor.pids_by_query(sort, '', '', 20, tag))
+        state['content'] = _keyed_posts(posts)
+        state['discussion_idx'][tag][sort] = list(state['content'].keys())
         state['tag_idx']['trending'] = await _get_top_trending_tags()
 
     # tag "explorer"
@@ -367,28 +396,6 @@ async def get_state(path: str):
     return state
 
 
-async def get_content(author: str, permlink: str):
-    """Get a single post object."""
-    _validate_account(author)
-    _validate_permlink(permlink)
-    post_id = _get_post_id(author, permlink)
-    if not post_id:
-        return {'id': 0, 'author': '', 'permlink': ''}
-    return _get_posts([post_id])[0]
-
-
-async def get_content_replies(parent: str, parent_permlink: str):
-    """Get a list of post objects based on parent."""
-    _validate_account(parent)
-    _validate_permlink(parent_permlink)
-    post_id = _get_post_id(parent, parent_permlink)
-    if not post_id:
-        return []
-    post_ids = query_col("SELECT id FROM hive_posts WHERE "
-                         "parent_id = %d AND is_deleted = '0'" % post_id)
-    if not post_ids:
-        return []
-    return _get_posts(post_ids)
 
 @cached(ttl=3600)
 async def _get_top_trending_tags():
@@ -423,6 +430,17 @@ async def _get_trending_tags():
 
     return out
 
+
+def _legacy_amount(value):
+    """Return a steem-style amount string given a (numeric, asset-str)."""
+    if isinstance(value, str):
+        return value # already legacy
+    amount, asset = parse_amount(value)
+    prec = {'SBD': 3, 'STEEM': 3, 'VESTS': 6}[asset]
+    tmpl = ("%%.%df %%s" % prec)
+    return tmpl % (amount, asset)
+
+
 def _get_props_lite():
     """Return a minimal version of get_dynamic_global_properties data."""
     raw = json.loads(query_one("SELECT dgpo FROM hive_state"))
@@ -452,16 +470,12 @@ def _get_feed_price():
 
 def _load_discussion_recursive(author, permlink):
     """`get_state`-compatible recursive thread loader."""
-    _validate_account(author)
-    _validate_permlink(permlink)
     post_id = _get_post_id(author, permlink)
-    if not post_id:
-        return {}
-    return _load_posts_recursive([post_id])
+    return _load_posts_recursive([post_id]) if post_id else {}
 
 def _load_posts_recursive(post_ids):
     """Recursive post loader used by `_load_discussion_recursive`."""
-    posts = _get_posts(post_ids)
+    posts = load_posts(post_ids)
 
     out = {}
     for post, post_id in zip(posts, post_ids):
@@ -476,21 +490,7 @@ def _load_posts_recursive(post_ids):
 
     return out
 
-def _load_accounts(names):
-    """`get_accounts`-style lookup for `get_state` compat layer."""
-    sql = """SELECT id, name, display_name, about, reputation
-               FROM hive_accounts WHERE name IN :names"""
-    rows = query_all(sql, names=tuple(names))
-    return [_condenser_account(row) for row in rows]
 
-def _condenser_account(row):
-    """Convert an internal account record into legacy-steemd style."""
-    return {
-        'name': row['name'],
-        'reputation': _rep_to_raw(row['reputation']),
-        'json_metadata': json.dumps({
-            'profile': {'name': row['display_name'],
-                        'about': row['about']}})}
 
 def _validate_account(name, allow_empty=False):
     assert isinstance(name, str), "account must be string; received: %s" % name
@@ -536,137 +536,3 @@ def _get_post_id(author, permlink):
         print("_get_post_id - post was deleted %s/%s" % (author, permlink))
         return None
     return _id
-
-def _get_posts(ids, truncate_body=0):
-    """Given an array of post ids, returns full objects in the same order."""
-    if not ids:
-        caller = inspect.stack()[1][3]
-        print("empty result for %s" % caller)
-        return []
-
-    sql = """
-    SELECT post_id, author, permlink, title, body, promoted, payout, created_at,
-           payout_at, is_paidout, rshares, raw_json, category, depth, json,
-           children, votes, author_rep,
-
-           preview, img_url, is_nsfw
-      FROM hive_posts_cache WHERE post_id IN :ids
-    """
-
-    # key by id so we can return sorted by input order
-    posts_by_id = {}
-    for row in query_all(sql, ids=tuple(ids)):
-        row = dict(row)
-        post = _condenser_post_object(row, truncate_body=truncate_body)
-        posts_by_id[row['post_id']] = post
-
-    # in rare cases of cache inconsistency, recover and warn
-    missed = set(ids) - posts_by_id.keys()
-    if missed:
-        print("WARNING: get_posts do not exist in cache: {}".format(missed))
-        for _id in missed:
-            ids.remove(_id)
-
-    return [posts_by_id[_id] for _id in ids]
-
-
-def _condenser_post_object(row, truncate_body=0):
-    """Given a hive_posts_cache row, create a legacy-style post object."""
-    paid = row['is_paidout']
-
-    post = {}
-    post['post_id'] = row['post_id']
-    post['author'] = row['author']
-    post['permlink'] = row['permlink']
-    post['category'] = row['category']
-    post['parent_permlink'] = ''
-    post['parent_author'] = ''
-
-    post['title'] = row['title']
-    post['body'] = row['body'][0:truncate_body] if truncate_body else row['body']
-    post['json_metadata'] = row['json']
-
-    post['created'] = _json_date(row['created_at'])
-    post['depth'] = row['depth']
-    post['children'] = row['children']
-    post['net_rshares'] = row['rshares']
-
-    post['last_payout'] = _json_date(row['payout_at'] if paid else None)
-    post['cashout_time'] = _json_date(None if paid else row['payout_at'])
-    post['total_payout_value'] = _amount(row['payout'] if paid else 0)
-    post['curator_payout_value'] = _amount(0)
-    post['pending_payout_value'] = _amount(0 if paid else row['payout'])
-    post['promoted'] = "%.3f SBD" % row['promoted']
-
-    post['replies'] = []
-    post['body_length'] = len(row['body'])
-    post['active_votes'] = _hydrate_active_votes(row['votes'])
-    post['author_reputation'] = _rep_to_raw(row['author_rep'])
-
-    # import fields from legacy object
-    assert row['raw_json']
-    assert len(row['raw_json']) > 32
-    raw_json = json.loads(row['raw_json'])
-
-    if row['depth'] > 0:
-        post['parent_permlink'] = raw_json['parent_permlink']
-        post['parent_author'] = raw_json['parent_author']
-
-    post['root_title'] = raw_json['root_title']
-    post['max_accepted_payout'] = raw_json['max_accepted_payout']
-    post['percent_steem_dollars'] = raw_json['percent_steem_dollars']
-    post['url'] = raw_json['url']
-
-    # not used by condenser, but may be useful
-    #post['net_votes'] = post['total_votes'] - row['up_votes']
-    #post['allow_replies'] = raw_json['allow_replies']
-    #post['allow_votes'] = raw_json['allow_votes']
-    #post['allow_curation_rewards'] = raw_json['allow_curation_rewards']
-    #post['beneficiaries'] = raw_json['beneficiaries']
-    #post['curator_payout_value'] = raw_json['curator_payout_value'] if paid else _amount(0)
-    #curator_payout = amount(raw_json['curator_payout_value'])
-    #post['total_payout_value'] = _amount(row['payout'] - curator_payout) if paid else _amount(0)
-
-    return post
-
-def _amount(amount, asset='SBD'):
-    """Return a steem-style amount string given a (numeric, asset-str)."""
-    if asset == 'SBD':
-        return "%.3f SBD" % amount
-    raise Exception("unexpected %s" % asset)
-
-def _hydrate_active_votes(vote_csv):
-    """Convert minimal CSV representation into steemd-style object."""
-    if not vote_csv:
-        return []
-    cols = 'voter,rshares,percent,reputation'.split(',')
-    votes = vote_csv.split("\n")
-    return [dict(zip(cols, line.split(','))) for line in votes]
-
-def _json_date(date=None):
-    """Given a db datetime, return a steemd/json-friendly version."""
-    if not date:
-        return '1969-12-31T23:59:59'
-    return 'T'.join(str(date).split(' '))
-
-def _rep_to_raw(rep):
-    """Convert a UI-ready rep score back into its approx raw value."""
-    if not isinstance(rep, (str, float, int)):
-        return 0
-    rep = float(rep) - 25
-    rep = rep / 9
-    rep = rep + 9
-    sign = 1 if rep >= 0 else -1
-    return int(sign * pow(10, rep))
-
-def _legacy_amount(value):
-    """Return a steem-style amount string given a (numeric, asset-str)."""
-    if isinstance(value, str):
-        return value # already legacy
-    amount, asset = parse_amount(value)
-    prec = {'SBD': 3, 'STEEM': 3, 'VESTS': 6}[asset]
-    tmpl = ("%%.%df %%s" % prec)
-    return tmpl % (amount, asset)
-
-if __name__ == '__main__':
-    print(_load_discussion_recursive('roadscape', 'hello-world'))
