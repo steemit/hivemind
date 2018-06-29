543b30c4ee10cec67cd99c5ca511ab84b32d171f
roadscape <roadscape@users.noreply.github.com>
2018-06-29 14:11:37 -0500

    get_state tests

---
+++ tests/test_server_condenser_api.py
@@ -0,0 +1,33 @@
+import pytest
+from hive.server.condenser_api.get_state import get_state
+
+@pytest.mark.asyncio
+async def test_get_state():
+    ret = await get_state('/trending')
+    assert 'discussion_idx' in ret
+
+    assert await get_state('trending')
+    assert await get_state('promoted')
+    assert await get_state('created')
+    assert await get_state('hot')
+
+    assert await get_state('@test-safari')
+    assert await get_state('@test-safari/feed')
+    assert await get_state('@test-safari/comments')
+    assert await get_state('@test-safari/recent-replies')
+
+    assert await get_state('spam/@test-safari/1ncq2-may-spam')
+
+    assert await get_state('trending/blockchain')
+
+    assert await get_state('tags')
+
+    with pytest.raises(AssertionError):
+        await get_state('trending/blockchain/xxx')
+
+    with pytest.raises(AssertionError):
+        await get_state('tags/xxx')
+
+    with pytest.raises(Exception):
+        await get_state('witnesses')
+
