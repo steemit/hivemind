907896aec5b9d48f65f2160b9d6bcdcbb94ee47f
roadscape <roadscape@users.noreply.github.com>
2019-07-10 15:59:17 -0500

    more community op validation

--- hive/indexer/community.py
+++ hive/indexer/community.py
@@ -1,5 +1,7 @@
 """[WIP] Process community ops."""
 
+#pylint: disable=too-many-lines
+
 import logging
 import re
 import ujson as json
@@ -64,15 +66,20 @@ class Community:
         return bool(DB.query_one(sql, name=name))
 
     @classmethod
+    def get_id(cls, name):
+        """Given a community name, get its internal id."""
+        sql = "SELECT id FROM hive_communities WHERE name = :name"
+        return DB.query_one(sql, name=name)
+
+    @classmethod
     def get_user_role(cls, community, account):
         """Get user role within a specific community."""
-        if account == community:
-            return ROLE_OWNER
-
         return DB.query_one("""SELECT role_id FROM hive_roles
                                 WHERE community = :community
                                   AND account = :account
-                                LIMIT 1""") or ROLE_GUEST
+                                LIMIT 1""",
+                            community=community,
+                            account=account) or ROLE_GUEST
 
     @classmethod
     def is_post_valid(cls, community, comment_op: dict):
@@ -87,6 +94,8 @@ class Community:
         role = cls.get_user_role(community, comment_op['author'])
         type_id = int(community[5])
 
+        # TODO: (1.5) check that beneficiaries are valid
+
         if type_id == TYPE_JOURNAL:
             if not comment_op['parent_author']:
                 return role >= ROLE_MEMBER
@@ -94,132 +103,149 @@ class Community:
             return role >= ROLE_MEMBER
         return role >= ROLE_GUEST
 
-
-
-def is_permitted(account: str, community: str, action: str, op: dict) -> bool:
-    """ Check if an account is allowed to perform an action in a given community."""
-    #pylint: disable=too-many-return-statements,too-many-branches
-    role = Community.get_user_role(community, account)
-
-    if action == 'setRole':
-        if role < ROLE_MOD:
-            return False
-        assert op['role'] in ROLES.keys()
-        new_role = ROLES[op['role']]
-        if account == op['account']:
-            return new_role < role # demote self
-        else:
-            other_role = Community.get_user_role(community, op['account'])
-            if role <= other_role:
-                return False # cannot demote at or above rank
-            return True
-
-    if action == 'updateSettings':
-        return role >= ROLE_ADMIN
-    if action == 'setUserTitle':
-        return role >= ROLE_MOD
-    if action == 'mutePost':
-        return role >= ROLE_MOD
-    if action == 'unmutePost':
-        return role >= ROLE_MOD
-    if action == 'pinPost':
-        return role >= ROLE_MOD
-    if action == 'unpinPost':
-        return role >= ROLE_MOD
-    if action == 'flagPost':
-        return role >= ROLE_GUEST
-    if action == 'subscribe':
-        return True
-    if action == 'unsubscribe':
-        return True
-
-    raise Exception('unhandled action `%s`' % action)
+def read_key_str(op, key):
+    """Reads a key from a dict, ensuring non-blank str if present."""
+    if key in op:
+        assert isinstance(op[key], str), 'key `%s` was not str' % key
+        assert op[key], 'key `%s` was blank' % key
+        return op[key]
+    return None
 
 # community methods
 # -----------------
-def process_json_community_op(account, op_json, date):
+def process_json_community_op(actor, op_json, date):
     """Validates community op and apply state changes to db."""
-    #pylint: disable=line-too-long,unused-variable,too-many-branches
-    cmd_name, cmd_op = op_json  # ['flagPost', {community: '', author: '', ...}]
+    #pylint: disable=line-too-long,unused-variable,too-many-branches,too-many-locals,too-many-statements
 
-    if (cmd_name not in COMMANDS
-            or 'community' not in cmd_op
-            or not isinstance(cmd_op['community'], str)
-            or not Accounts.exists(cmd_op['community'])
-            or not Community.exists(cmd_op['community'])):
-        return
+    action, op = op_json  # ['flagPost', {community: '', author: '', ...}]
+    actor_id = Accounts.get_id(actor)
 
-    community = cmd_op['community']
+    # validate operation
+    assert action in COMMANDS, 'invalid op: `%s`' % action
 
-    log.warning("community op from %s @ %s -- %s", account, date, op_json)
+    # validate community
+    community = read_key_str(op, 'community')
+    assert community, 'must name a community'
+    assert Accounts.exists(community), 'invalid name `%s`' % community
+    community_id = Community.get_id(community)
+    assert community_id, 'community `%s` does not exist' % community
+
+    # get actor's role
+    actor_role = Community.get_user_role(community, actor)
+
+    # if present: validate account
+    account = read_key_str(op, 'account')
+    account_id = None
+    if account:
+        assert Accounts.exists(account), 'account `%s` not found' % account
+        account_id = Accounts.get_id(account)
+
+    # if present: validate permlink
+    permlink = read_key_str(op, 'permlink')
+    post_id = None
+    depth = None
+    if permlink:
+        assert account, 'permlink requires named account'
+        post_id, depth = Posts.get_id_and_depth(account, permlink)
+        assert post_id, 'invalid post: %s/%s' % (account, permlink)
+        # TODO: assert post belongs to community
+
+    role = read_key_str(op, 'role')
+    new_role = None
+    if role:
+        assert role in ROLES, 'invalid role'
+        new_role = ROLES[role]
 
     # validate permissions
-    if not is_permitted(account, community, cmd_name, cmd_op):
-        return
+    if action == 'setRole':
+        assert actor_role >= ROLE_MOD, 'only mods and up can alter roles'
+        assert actor_role > new_role, 'cannot promote to or above own rank'
 
-    # If command references a post, ensure it's valid
-    post_id, depth = Posts.get_id_and_depth(cmd_op.get('author'), cmd_op.get('permlink'))
-    if not post_id:
-        return
+        if actor != account:
+            account_role = Community.get_user_role(community, account)
+            assert account_role < actor_role, 'cannot modify higher-role user'
+            assert account_role != new_role, 'user is already `%s`' % op['role']
 
-    # If command references an account, ensure it's valid
-    account_id = Accounts.get_id(cmd_op.get('account'))
+    if action == 'updateSettings':
+        assert actor_role >= ROLE_ADMIN, 'only mods can update settings'
+    if action == 'setUserTitle':
+        assert actor_role >= ROLE_MOD, 'only mods can set user titles'
+    if action == 'mutePost':
+        assert actor_role >= ROLE_MOD, 'only mods can mute posts'
+    if action == 'unmutePost':
+        assert actor_role >= ROLE_MOD, 'only mods can unmute posts'
+    if action == 'pinPost':
+        assert actor_role >= ROLE_MOD, 'only mods can pin posts'
+    if action == 'unpinPost':
+        assert actor_role >= ROLE_MOD, 'only mods can unpin posts'
+    if action == 'flagPost':
+        assert actor_role > ROLE_MUTED, 'muted users cannot flag posts'
+    if action == 'subscribe':
+        pass
+    if action == 'unsubscribe':
+        pass
 
 
+
+
+    log.warning("valid community op from %s @ %s -- %s", actor, date, op_json)
 
     # Community-level commands
     # ----------------
 
-    if cmd_name == 'updateSettings':
-        settings = cmd_op['settings']
+    if action == 'updateSettings':
+        settings = op['settings']
         DB.query("UPDATE hive_communities SET settings = :settings WHERE name = :community",
                  community=community, settings=json.dumps(settings))
 
-    if cmd_name == 'subscribe':
+    if action == 'subscribe':
         DB.query("INSERT INTO hive_subscriptions (account, community) VALUES (:account, :community)",
-                 account=account, community=community)
-    if cmd_name == 'unsubscribe':
+                 account=actor, community=community)
+
+    if action == 'unsubscribe':
         DB.query("DELETE FROM hive_subscriptions WHERE account = :account AND community = :community",
-                 account=account, community=community)
+                 account=actor, community=community)
 
     # Account-level actions
     # ----------------
-    if cmd_name == 'setRole':
+    if action == 'setRole':
         assert account_id
-        role_id = ROLES[cmd_op['role']]
-        assert role_id
+        role = read_key_str(op, 'role')
+        assert role, 'no role specified'
+        role_id = ROLES[role]
+        assert role_id, 'invalid role `%s`' % role
         DB.query("UPDATE hive_roles SET role_id = :role_id WHERE account = :account AND community = :community",
                  role_id=role_id, account=account, community=community)
 
-    if cmd_name == 'setUserTitle':
+    if action == 'setUserTitle':
         assert account_id
-        title = cmd_op['title']
-        DB.query("UPDATE hive_members SET title = :title WHERE account = :account AND community = :community",
+        title = read_key_str(op, 'title')
+        DB.query("UPDATE hive_roles SET title = :title WHERE account = :account AND community = :community",
                  title=title, account=account, community=community)
 
 
     # MOD POST Actions
     # ----------------
-    if cmd_name == 'mutePost':
-        assert post_id
+    if action == 'mutePost':
+        assert post_id, 'no post specified'
         DB.query("UPDATE hive_posts SET is_muted = 1 WHERE id = :id", id=post_id)
 
-    if cmd_name == 'unmutePost':
-        assert post_id
+    if action == 'unmutePost':
+        assert post_id, 'no post specified'
         DB.query("UPDATE hive_posts SET is_muted = 0 WHERE id = :id", id=post_id)
 
-    if cmd_name == 'pinPost':
-        assert post_id
+    if action == 'pinPost':
+        assert post_id, 'no post specified'
         DB.query("UPDATE hive_posts SET is_pinned = 1 WHERE id = :id", id=post_id)
 
-    if cmd_name == 'unpinPost':
-        assert post_id
+    if action == 'unpinPost':
+        assert post_id, 'no post specified'
         DB.query("UPDATE hive_posts SET is_pinned = 0 WHERE id = :id", id=post_id)
 
     # GUEST POST Actions
     # ------------------
-    if cmd_name == 'flagPost':
-        assert post_id
+    if action == 'flagPost':
+        assert post_id, 'no post specified'
         DB.query("INSERT INTO hive_flags (account, community, author, permlink, comment, created_at) VALUES (:account, :community, :author, :permlink, :comment, :date)")
 
     # track success (TODO: failures as well?)
