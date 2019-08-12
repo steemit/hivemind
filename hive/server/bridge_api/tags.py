173683885da1088b3740ed176b0723457ca38b97
roadscape <roadscape@users.noreply.github.com>
2019-08-12 18:47:52 -0500

    remove pagination from bridge.get_trending_tags

--- hive/server/bridge_api/tags.py
+++ hive/server/bridge_api/tags.py
@@ -1,7 +1,7 @@
 """condenser_api trending tag fetching methods"""
 
 from aiocache import cached
-from hive.server.condenser_api.common import (return_error_info, valid_tag, valid_limit)
+from hive.server.condenser_api.common import (return_error_info, valid_limit)
 
 @return_error_info
 @cached(ttl=7200, timeout=1200)
@@ -19,22 +19,10 @@ async def get_top_trending_tags_summary(context, limit=50):
 
 @return_error_info
 @cached(ttl=3600, timeout=1200)
-async def get_trending_tags(context, start_tag: str = '', limit: int = 250):
-    """Get top 250 trending tags among pending posts, with stats."""
-
+async def get_trending_tags(context, start_tag='', limit: int = 250):
+    """Get top trending tags among pending posts, with stats."""
+    assert not start_tag, 'pagination not supported'
     limit = valid_limit(limit, ubound=250)
-    start_tag = valid_tag(start_tag or '', allow_empty=True)
-
-    if start_tag:
-        seek = """
-          HAVING SUM(payout) <= (
-            SELECT SUM(payout)
-              FROM hive_posts_cache
-             WHERE is_paidout = '0'
-               AND category = :start_tag)
-        """
-    else:
-        seek = ''
 
     sql = """
       SELECT category,
@@ -43,13 +31,13 @@ async def get_trending_tags(context, start_tag: str = '', limit: int = 250):
              SUM(payout) AS total_payouts
         FROM hive_posts_cache
        WHERE is_paidout = '0'
-    GROUP BY category %s
+    GROUP BY category
     ORDER BY SUM(payout) DESC
        LIMIT :limit
-    """ % seek
+    """
 
     out = []
-    for row in await context['db'].query_all(sql, limit=limit, start_tag=start_tag):
+    for row in await context['db'].query_all(sql, limit=limit):
         out.append({
             'name': row['category'],
             'comments': row['total_posts'] - row['top_posts'],
