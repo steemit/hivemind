3d87c01ccf5c173f191f043ca7da2776345a4f00
roadscape <roadscape@users.noreply.github.com>
2019-09-12 11:33:47 -0500

    cache get_state props for 30 mins

--- hive/server/condenser_api/get_state.py
+++ hive/server/condenser_api/get_state.py
@@ -4,6 +4,7 @@
 import logging
 from collections import OrderedDict
 import ujson as json
+from aiocache import cached
 
 from hive.utils.normalize import legacy_amount
 from hive.server.common.mutes import Mutes
@@ -277,11 +278,13 @@ async def _load_discussion(db, author, permlink):
     # return all nodes keyed by ref
     return {refs[pid]: post for pid, post in posts.items()}
 
+@cached(ttl=1800, timeout=1200)
 async def _get_feed_price(db):
     """Get a steemd-style ratio object representing feed price."""
     price = await db.query_one("SELECT usd_per_steem FROM hive_state")
     return {"base": "%.3f SBD" % price, "quote": "1.000 STEEM"}
 
+@cached(ttl=1800, timeout=1200)
 async def _get_props_lite(db):
     """Return a minimal version of get_dynamic_global_properties data."""
     raw = json.loads(await db.query_one("SELECT dgpo FROM hive_state"))
