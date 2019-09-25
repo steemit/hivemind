363a8d74172129e55e54f95ea5766196d5462aaf
roadscape <roadscape@users.noreply.github.com>
2019-09-24 19:22:36 -0500

    fix acct_notifs last_id param

--- hive/server/hive_api/notify.py
+++ hive/server/hive_api/notify.py
@@ -45,7 +45,7 @@ async def account_notifications(context, account, min_score=0, last_id=None, lim
     limit = valid_limit(limit, 100)
     account_id = await get_account_id(db, account)
 
-    seek = ' AND id < :last_id' if last_id else ''
+    seek = ' AND hn.id < :last_id' if last_id else ''
     col = 'community_id' if account[:5] == 'hive-' else 'dst_id'
     sql = _notifs_sql(col + " = :dst_id" + seek)
 
