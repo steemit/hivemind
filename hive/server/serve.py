8166bee3d5b52ca2edd3766ad0f8920847ef39ac
roadscape <roadscape@users.noreply.github.com>
2019-10-18 15:52:31 -0500

    remove bridge_api get_state

--- hive/server/serve.py
+++ hive/server/serve.py
@@ -18,7 +18,6 @@ from hive.server.condenser_api.call import call as condenser_api_call
 from hive.server.common.mutes import Mutes
 
 from hive.server.bridge_api import methods as bridge_api
-from hive.server.bridge_api.get_state import get_state as bridge_api_get_state
 from hive.server.bridge_api.thread import get_discussion as bridge_api_get_discussion
 from hive.server.bridge_api.support import normalize_post as bridge_api_normalize_post
 from hive.server.bridge_api.support import get_post_header as bridge_api_get_post_header
@@ -113,7 +112,6 @@ def build_methods():
     methods.add(**{'bridge.' + method.__name__: method for method in (
         bridge_api_normalize_post,
         bridge_api_get_post_header,
-        bridge_api_get_state,
         bridge_api_get_discussion,
         bridge_api.get_account_posts,
         bridge_api.get_ranked_posts,
