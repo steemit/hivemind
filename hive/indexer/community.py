c94ce48a36be6db08f344c4cf60d21b9eac1d845
roadscape <roadscape@users.noreply.github.com>
2019-10-22 10:25:39 -0500

    add maintain num_pending

--- hive/indexer/community.py
+++ hive/indexer/community.py
@@ -208,27 +208,28 @@ class Community:
     @classmethod
     def recalc_pending_payouts(cls):
         """Update all pending payout and rank fields."""
-        sql = """SELECT c.id, COALESCE(p.pending, 0) pending
+        sql = """SELECT c.id, COALESCE(p.ttl, 0) ttl, COALESCE(p.cnt, 0) cnt
                    FROM hive_communities c
               LEFT JOIN (
-                             SELECT community_id, SUM(payout) pending
+                             SELECT community_id, SUM(payout) ttl, COUNT(*) cnt
                                FROM hive_posts_cache
                               WHERE community_id IS NOT NULL
                                 AND is_paidout = '0'
                            GROUP BY community_id
                         ) p
                      ON p.community_id = c.id
-               ORDER BY COALESCE(p.pending, -1) DESC,
+               ORDER BY COALESCE(p.ttl, -1) DESC,
                         c.subscribers DESC,
+                        COALESCE(p.cnt, -1) DESC,
                         (CASE WHEN c.title = '' THEN 1 ELSE 0 END)
         """
 
         for rank, row in enumerate(DB.query_all(sql)):
-            community_id, pending = row
+            community_id, ttl, cnt = row
             sql = """UPDATE hive_communities
-                        SET sum_pending = :pending, rank = :rank
+                        SET sum_pending = :ttl, num_pending = :cnt, rank = :rank
                       WHERE id = :id"""
-            DB.query(sql, id=community_id, pending=pending, rank=rank+1)
+            DB.query(sql, id=community_id, ttl=ttl, cnt=cnt, rank=rank+1)
 
 class CommunityOp:
     """Handles validating and processing of community custom_json ops."""
